629ded78742409528458231d6e39792508ca12d5
subha300622 <subhamano3006@gmail.com>
2026-07-16 11:11:26 +0530

    feat: support dynamic host resolution for both postgres:// and jdbc:postgresql:// formats

--- src/main/java/com/example/ems/EmsBackendApplication.java
+++ src/main/java/com/example/ems/EmsBackendApplication.java
@@ -17,51 +17,64 @@ public class EmsBackendApplication {
         if (rawUrl == null || rawUrl.isBlank()) {
             rawUrl = System.getenv("DATABASE_URL");
         }
-        if (rawUrl != null && (rawUrl.startsWith("postgres://") || rawUrl.startsWith("postgresql://"))) {
+        if (rawUrl != null && !rawUrl.isBlank()) {
             try {
-                String cleanUrl = rawUrl.substring(rawUrl.indexOf("://") + 3);
-                String[] authAndHost = cleanUrl.split("@");
-                if (authAndHost.length == 2) {
-                    String[] credentials = authAndHost[0].split(":");
-                    String username = credentials[0];
-                    String password = credentials.length > 1 ? credentials[1] : "";
-                    
-                    String hostAndDb = authAndHost[1];
-                    
-                    String hostAndPort;
-                    String database = "";
-                    int slashIdx = hostAndDb.indexOf('/');
-                    if (slashIdx != -1) {
-                        hostAndPort = hostAndDb.substring(0, slashIdx);
-                        database = hostAndDb.substring(slashIdx);
-                    } else {
-                        hostAndPort = hostAndDb;
+                if (rawUrl.startsWith("postgres://") || rawUrl.startsWith("postgresql://")) {
+                    String cleanUrl = rawUrl.substring(rawUrl.indexOf("://") + 3);
+                    String[] authAndHost = cleanUrl.split("@");
+                    if (authAndHost.length == 2) {
+                        String[] credentials = authAndHost[0].split(":");
+                        String username = credentials[0];
+                        String password = credentials.length > 1 ? credentials[1] : "";
+                        
+                        String hostAndDb = authAndHost[1];
+                        String resolvedHostAndDb = resolveHostAndDb(hostAndDb);
+                        String jdbcUrl = "jdbc:postgresql://" + resolvedHostAndDb;
+                        
+                        System.setProperty("spring.datasource.url", jdbcUrl);
+                        System.setProperty("spring.datasource.username", username);
+                        System.setProperty("spring.datasource.password", password);
+                        System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+                        System.out.println("Dynamically configured PostgreSQL DataSource from postgres:// env variable.");
                     }
-                    
-                    String host;
-                    String port = "";
-                    int colonIdx = hostAndPort.indexOf(':');
-                    if (colonIdx != -1) {
-                        host = hostAndPort.substring(0, colonIdx);
-                        port = hostAndPort.substring(colonIdx);
-                    } else {
-                        host = hostAndPort;
-                    }
-                    
-                    String resolvedHost = resolveHostIfNeeded(host);
-                    String jdbcUrl = "jdbc:postgresql://" + resolvedHost + port + database;
+                } else if (rawUrl.startsWith("jdbc:postgresql://")) {
+                    String hostAndDb = rawUrl.substring("jdbc:postgresql://".length());
+                    String resolvedHostAndDb = resolveHostAndDb(hostAndDb);
+                    String jdbcUrl = "jdbc:postgresql://" + resolvedHostAndDb;
                     
                     System.setProperty("spring.datasource.url", jdbcUrl);
-                    System.setProperty("spring.datasource.username", username);
-                    System.setProperty("spring.datasource.password", password);
-                    System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
-                    System.out.println("Dynamically configured PostgreSQL DataSource from environment variable.");
+                    System.out.println("Dynamically configured PostgreSQL DataSource from jdbc:// env variable.");
                 }
             } catch (Exception e) {
                 System.err.println("Failed to parse database URL: " + e.getMessage());
             }
         }
         SpringApplication.run(EmsBackendApplication.class, args);
+    }
+
+    private static String resolveHostAndDb(String hostAndDb) {
+        String hostAndPort;
+        String database = "";
+        int slashIdx = hostAndDb.indexOf('/');
+        if (slashIdx != -1) {
+            hostAndPort = hostAndDb.substring(0, slashIdx);
+            database = hostAndDb.substring(slashIdx);
+        } else {
+            hostAndPort = hostAndDb;
+        }
+        
+        String host;
+        String port = "";
+        int colonIdx = hostAndPort.indexOf(':');
+        if (colonIdx != -1) {
+            host = hostAndPort.substring(0, colonIdx);
+            port = hostAndPort.substring(colonIdx);
+        } else {
+            host = hostAndPort;
+        }
+        
+        String resolvedHost = resolveHostIfNeeded(host);
+        return resolvedHost + port + database;
     }
 
     private static String resolveHostIfNeeded(String host) {
